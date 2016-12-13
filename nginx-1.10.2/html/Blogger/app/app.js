ced154acdb882234839e60abc34423b20d43f7c6
murali <gmurali7891@gmail.com>
2016-12-12 21:17:03 -0800

    code added

---
+++ nginx-1.10.2/html/Blogger/app/app.js
@@ -0,0 +1,10 @@
+(function(){
+    angular.module("LeeCopper",[]);
+    angular.module("LeeCopper")
+    .config(["$stateProvider","$UrlRouterprovider",
+    function($stateProvider,$UrlRouterprovider){
+        var productObj = {
+            templateUrl: "app/"
+        }
+    }])
+})();
